cb18fc424e12ae9c4f6fc4273c5fc67264c159db
Don Assamongkol <don.assamongkol1@gmail.com>
2022-09-30 13:19:09 -0500

    changed to structs + ptrs

--- parallel_hw1.c
+++ parallel_hw1.c
@@ -7,57 +7,58 @@
 #define MAX_INT_LENGTH 1000
 #define MAX_STRING_LENGTH 10
 
-typedef struct adj {
+typedef struct graph {
     int N;
-    int **weights;
-} adj_t;
+    int **adjacency_matrix;
+} graph_t;
 
 int main() {
     printf("Running Script...\n");
 
     /* make adjacency matrix from reading in input */
-    // adj_t *adjacency_matrix_ = calloc(1, sizeof(adj_t));
-
+    graph_t *graph = calloc(1, sizeof(graph_t));
     FILE *input_file = fopen("sample_input.txt", "r");
     char line[MAX_LINE_LENGTH];
 
     fgets(line, MAX_LINE_LENGTH, input_file);  // reads the first line of the file
-    int N = line[0] - '0';                     // cast char to int
+    graph->N = line[0] - '0';                  // cast char to int
+    graph->adjacency_matrix = calloc(graph->N, sizeof(int *));
 
-    int adjacency_matrix[N][N];
-
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < graph->N; i++) {
         fgets(line, MAX_LINE_LENGTH, input_file);
 
-        char delimeter[] = " ";
+        int *row = calloc(graph->N, sizeof(int));
+        char *delimeter = " ";
         int j = 0;
         char *ptr = strtok(line, delimeter);
+
         while (ptr != NULL) {
             if (ptr[0] == 'i') {
-                adjacency_matrix[i][j] = INFINITY;
+                row[j] = INFINITY;
             } else {
-                adjacency_matrix[i][j] = atoi(ptr);  // convert the ptr str to an int
+                row[j] = atoi(ptr);
             }
             ptr = strtok(NULL, delimeter);
             j += 1;
         }
+        graph->adjacency_matrix[i] = row;
     }
 
     /* run the algorithm */
-    int distances[N][N];
-    for (int u = 0; u < N; u++) {
-        for (int v = 0; v < N; v++) {
+    int distances[graph->N][graph->N];
+    for (int u = 0; u < graph->N; u++) {
+        for (int v = 0; v < graph->N; v++) {
             if (u == v) {
                 distances[u][u] = 0;  // assume that weight of edge to self is 0, this should be what the input file has
             } else {
-                distances[u][v] = adjacency_matrix[u][v];
+                distances[u][v] = graph->adjacency_matrix[u][v];
             }
         }
     }
 
-    for (int k = 0; k < N; k++) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+    for (int k = 0; k < graph->N; k++) {
+        for (int i = 0; i < graph->N; i++) {
+            for (int j = 0; j < graph->N; j++) {
                 if (distances[i][j] > distances[i][k] + distances[k][j]) {
                     distances[i][j] = distances[i][k] + distances[k][j];
                 }
@@ -65,7 +66,7 @@ int main() {
         }
     }
 
-    /* output a txt file of the answer */
+    /* output a txt file that contains the answer */
     remove("sample_output.txt");
     FILE *output_file = fopen("sample_output.txt", "a");
     if (output_file == NULL) {
@@ -73,10 +74,10 @@ int main() {
         exit(1);
     }
 
-    for (int u = 0; u < N; u++) {
+    for (int u = 0; u < graph->N; u++) {
         char output_row_weights[MAX_LINE_LENGTH] = "";
 
-        for (int v = 0; v < N; v++) {
+        for (int v = 0; v < graph->N; v++) {
             if (distances[u][v] == INFINITY) {
                 strncat(output_row_weights, "i ", MAX_STRING_LENGTH);
             } else {
@@ -92,13 +93,3 @@ int main() {
 
     return 0;
 }
-
-/*
-Additional comments:
-int a[10][20] assigns space for 200 int-sized locations
-int *b[10] allocates space for 10 pointers. If you then have the pointers point
-    to arrays of 20 ints, i.e. arrays that have space for 20 ints, then you have your
-    2D array. You can see that in the latter case, you assign 210 memory locations
-    instead of 200
-
-*/
